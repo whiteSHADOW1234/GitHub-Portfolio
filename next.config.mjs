783966688b2ce74850d55db38a1eb782b16f9d0f
whiteSHADOW1234 <enkaihuang2021@gmail.com>
2025-07-15 18:57:49 +0800

    fix: ensure basePath and assetPrefix are set correctly in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/GitHub-Portfolio',
+    assetPrefix: '/GitHub-Portfolio',
 };
 
 export default nextConfig;
